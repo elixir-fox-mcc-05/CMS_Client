08b4e97bf2d4ed319840da838c65d556c921e02a
kalys <61675998+kalyskhairy@users.noreply.github.com>
2020-05-14 01:55:54 +0700

    intial commit

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="../assets/logoSneaker.png" style="width: 70%;">
+    <Signin />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Signin from '@/components/Signin.vue'
+
+export default {
+  name: 'LandingPage',
+  components: {
+    Signin
+  }
+}
+</script>
