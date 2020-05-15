5ce6b1dd43177e12a7d5f35da6169511dfad8910
farissm <farismuhammad2198@gmail.com>
2020-05-15 22:37:32 +0700

    todo: banyak

---
+++ client/src/views/Login.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="simple-login-container">
+    <h2>Admin Login</h2>
+    <div class="row">
+        <div class="col-md-12 form-group">
+            <input type="text" class="form-control" placeholder="Username">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 form-group">
+            <input type="password" placeholder="Enter your Password" class="form-control">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 form-group">
+            <input type="submit" class="btn btn-block btn-login" placeholder="Enter your Password" >
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Login'
+}
+</script>
+<style>
+body{
+    background-color:#fff;
+    font-size:14px;
+    color:#fff;
+}
+.simple-login-container{
+    width:300px;
+    max-width:100%;
+    margin:50px auto;
+}
+.simple-login-container h2{
+    text-align:center;
+    font-size:20px;
+}
+
+.simple-login-container .btn-login{
+    background-color:#5a5e5e;
+    color:#fff;
+}
+a{
+    color:#fff;
+}
+</style>
